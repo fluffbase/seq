9cffe5ac90a469f14665f5eee8e66cbe4e1c1bc5
William Wagner <william.wagner@gmail.com>
2025-05-29 14:55:22 -0400

    update set uid int for 32/64 bits

--- seq.go
+++ seq.go
@@ -153,11 +153,11 @@ func (e Exec) Run(envs ...Env) (*os.File, error) {
         }
         uid, _ := strconv.ParseInt(u.Uid, 10, 32)
         gid, _ := strconv.ParseInt(u.Gid, 10, 32)
-        cerr, errno := syscall.Setgid(gid)
+        cerr := syscall.Setgid((int)gid)
         if cerr != 0 {
             return nil, fmt.Errorf("failed to set GID %d due to error %d", gid, errno)
         }
-        cerr, errno = syscall.Setuid(uid)
+        cerr = syscall.Setuid((int)uid)
         if cerr != 0 {
             return nil, fmt.Errorf("failed to set UID %d due to error %d", uid, errno)
         }
